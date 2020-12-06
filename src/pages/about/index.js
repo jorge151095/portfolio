88b2001ef8584246dc2c2efa27f31a4ea61ba539
jorge.hg <jorgeantoniohernandezguzman@hotmail.com>
2020-12-05 21:50:11 -0600

    feat(about): Add styles in about page

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Element } from 'react-scroll';
+import { FaFacebook } from 'react-icons/fa';
 import { Photos } from './components/photos';
 import './about.scss';
 
@@ -10,8 +11,16 @@ export const About = () => {
       <div className="about">
         <Photos />
         <div className="text">
-          <span className="description"></span>
-          <span className="link"></span>
+          <span className="description">
+            <h4>Hi, I'm Jorge Hernández</h4>
+            <p>I'm a passionate front end developer with 4+ years developing software under both web and mobile environments.</p>
+            <p>When I'm not programming, I'm playing football soccer or playing the guitar.
+              I like to get in touch with my friends, travel around my country and taste new regional dishes.
+              I love the nature, that's because I live near to a big lagoon.
+            </p>
+            <p>As founder of Farmacias Hernández, in my free time I like to improve new solutions for my business, create and implement new tools to get new clients and provide excellent service.</p>
+          </span>
+          <span className="link"><a href="https://www.facebook.com/jorgeantonio.hernandez.77920"><FaFacebook/></a></span>
         </div>
       </div>
     </Element>
